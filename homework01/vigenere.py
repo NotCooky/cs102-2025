42a6a6784db35f96e23ca2cb60ee9f37cce1f807
Iliya <igabdullin2007@gmail.com>
2025-10-25 19:42:12 +0300

    Реализован vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,6 @@
+from operator import index
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,8 +12,36 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    extended_key = ""
+
+    if len(keyword) < len(plaintext):
+        for i in range(len(plaintext)):
+            extended_key += keyword[i % len(keyword)]
+    else:
+        extended_key = keyword
+
+    caps = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    lows = list('abcdefghijklmnopqrstuvwxyz')
+
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        key_char = extended_key[i]
+        if char in caps or char in lows:
+            if char in caps:
+                new_pos = (caps.index(char) + caps.index(key_char.upper())) % 26
+                ciphertext += caps[new_pos]
+            elif char in lows:
+                new_pos = (lows.index(char) + lows.index(key_char.lower())) % 26
+                ciphertext += lows[new_pos]
+        else:
+            ciphertext += char
+
+
     return ciphertext
+
+
+
+    return extended_key
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -24,5 +55,29 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    extended_key = ""
+
+    if len(keyword) < len(ciphertext):
+        for i in range(len(ciphertext)):
+            extended_key += keyword[i % len(keyword)]
+    else:
+        extended_key = keyword
+
+    caps = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    lows = list('abcdefghijklmnopqrstuvwxyz')
+
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        key_char = extended_key[i]
+        if char in caps or char in lows:
+            if char in caps:
+                new_pos = (caps.index(char) - caps.index(key_char.upper())) % 26
+                plaintext += caps[new_pos]
+            elif char in lows:
+                new_pos = (lows.index(char) - lows.index(key_char.lower())) % 26
+                plaintext += lows[new_pos]
+        else:
+            plaintext += char
+
     return plaintext
+
