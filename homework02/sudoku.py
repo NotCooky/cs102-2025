6acdf795cba846a47a183934e7be3a7596d93228
Iliya <igabdullin2007@gmail.com>
2025-12-04 02:51:24 +0300

    more formatting

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -105,7 +105,7 @@ def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[s
     return square
 
 
-def find_empty_positions(grid: tp.List[tp.List[str]],) -> tp.Optional[tp.Tuple[int, int]]:
+def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[int, int]]:
     """Найти первую свободную позицию в пазле
     >>> find_empty_positions([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']])
     (0, 2)
