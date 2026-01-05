31e850618f1e3bdbc5019934756405f8d3414a4c
Iliya <igabdullin2007@gmail.com>
2026-01-05 20:34:53 +0300

    black fix

--- homework04/life_proto.py
+++ homework04/life_proto.py
@@ -10,9 +10,7 @@ Grid = tp.List[Cells]
 
 
 class GameOfLife:
-    def __init__(
-        self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10
-    ) -> None:
+    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10) -> None:
         self.width = width
         self.height = height
         self.cell_size = cell_size
@@ -32,13 +30,9 @@ class GameOfLife:
     def draw_lines(self) -> None:
         """Отрисовать сетку"""
         for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(
-                self.screen, pygame.Color("black"), (x, 0), (x, self.height)
-            )
+            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(
-                self.screen, pygame.Color("black"), (0, y), (self.width, y)
-            )
+            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def run(self) -> None:
         """Запустить игру"""
@@ -148,10 +142,7 @@ class GameOfLife:
                 if i != 0 or j != 0:
                     new_row = row + i
                     new_col = col + j
-                    if (
-                        0 <= new_row < self.cell_height
-                        and 0 <= new_col < self.cell_width
-                    ):
+                    if 0 <= new_row < self.cell_height and 0 <= new_col < self.cell_width:
                         neighbours.append(self.grid[new_row][new_col])
         return neighbours
 
--- homework04/main.py
+++ homework04/main.py
@@ -3,7 +3,7 @@ from life_gui import GUI
 
 if __name__ == "__main__":
     life = GameOfLife((20, 20), randomize=True)
-    
+
     gui = GUI(life=life, cell_size=20, speed=10)
-    
+
     gui.run()
